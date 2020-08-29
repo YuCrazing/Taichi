111631e32f3acccc58d3db2f3b5ab872e4cdf61c
YuCrazing <723284893@qq.com>
2020-08-29 18:07:14 +0800

    -

--- fluid_2d/fluid_2d.py
+++ fluid_2d/fluid_2d.py
@@ -8,10 +8,8 @@ ti.init(arch=ti.gpu)
 
 
 n = 512
-dt = 0.05
+dt = 0.03
 dx = 1/n
-
-rho = 1
 
 # 1, 2, 3
 RK = 3
@@ -21,6 +19,12 @@ enable_BFECC = True
 
 #
 enable_clipping = True
+
+
+rho = 1
+jacobi_iters = 160
+
+
 
 colors = ti.Vector(3, dt=ti.f32, shape=(n, n))
 new_colors = ti.Vector(3, dt=ti.f32, shape=(n, n))
@@ -32,6 +36,8 @@ new_new_velocities = ti.Vector(2, dt=ti.f32, shape=(n, n))
 
 pressures = ti.var(dt=ti.f32, shape=(n, n))
 new_pressures = ti.var(dt=ti.f32, shape=(n, n))
+
+divergences = ti.var(dt=ti.f32, shape=(n, n))
 
 
 # screen center. The simualation area is (0, 0) to (1, 1)
@@ -55,24 +61,11 @@ def vel(p):
 
 @ti.kernel
 def init_color_field():
-	# super sampling
-	# for i, j in ti.ndrange(4*n, 4*n):
-	# 	d = ti.Vector([i/4*dx, j/4*dx]) - center
-	# 	if d.norm_sqr() < 0.1:
-	# 		x = i/4*dx
-	# 		if x < 1/3:
-	# 			colors[i//4, j//4] += ti.Vector([0.0, 0.0, 0.0])/16
-	# 		elif x < 2/3:
-	# 			colors[i//4, j//4] += ti.Vector([1.0, 1.0, 1.0])/16
-	# 		else:
-	# 			colors[i//4, j//4] += ti.Vector([1.0, 0.0, 0.0])/16
-	# 	else:
-	# 		colors[i//4, j//4] += ti.Vector([1.0, 1.0, 1.0])/16
-
 	# random
 	for i in ti.grouped(colors):
 		# colors[i] = ti.Vector([ti.random(), ti.random(), ti.random()])
 		colors[i] = ti.Vector([0.0, 0.0, 0.0])
+		# colors[i] = ti.Vector([1.0, 1.0, 1.0])
 
 
 @ti.kernel
@@ -174,11 +167,6 @@ def BFECC(field, new_field, new_new_field, dt):
 				if new_field[i][d] < mi[d] or new_field[i][d] > mx[d]:
 					new_field[i] = sample_bilinear(field, source_pos)
 
-			# if new_field[i].x < mi.x or new_field[i].y < mi.y or new_field[i].z < mi.z or new_field[i].x > mx.x or new_field[i].y > mx.y or new_field[i].z > mx.z:
-			# 	new_field[i] = sample_bilinear(field, source_pos)
-				
-				# runtime error
-				# break
 
 @ti.kernel
 def advect(field:ti.template(), new_field:ti.template(), new_new_field:ti.template(), dt:ti.f32):
@@ -187,92 +175,70 @@ def advect(field:ti.template(), new_field:ti.template(), new_new_field:ti.templa
 		BFECC(field, new_field, new_new_field, dt)
 	else:
 		semi_lagrangian(field, new_field, dt)
-		
-	for i in ti.grouped(field):
-		field[i] = new_field[i]
+
 
 
 @ti.kernel
-def jacobi():
+def solve_divergence():
 	for i, j in velocities:
-		# c = ti.Vector([i + stagger.x, j + stagger.y]) * dx
 		c = ti.Vector([i + stagger.x, j + stagger.y]) * dx
-		# l = c - ti.Vector([1, 0]) * dx
-		# r = c + ti.Vector([1, 0]) * dx
-		# d = c - ti.Vector([0, 1]) * dx
-		# u = c + ti.Vector([0, 1]) * dx
-		l = c - ti.Vector([stagger.x, 0]) * dx
-		r = c + ti.Vector([stagger.x, 0]) * dx
-		d = c - ti.Vector([0, stagger.y]) * dx
-		u = c + ti.Vector([0, stagger.y]) * dx
-
+		l = c - ti.Vector([1, 0]) * dx
+		r = c + ti.Vector([1, 0]) * dx
+		d = c - ti.Vector([0, 1]) * dx
+		u = c + ti.Vector([0, 1]) * dx
 		v_c = sample_bilinear(velocities, c)
 		v_l = sample_bilinear(velocities, l).x
 		v_r = sample_bilinear(velocities, r).x
 		v_d = sample_bilinear(velocities, d).y
 		v_u = sample_bilinear(velocities, u).y
 
-		# p_c = sample_bilinear(pressures, c)
+
+		if i == 0: 
+			v_l = -v_c.x
+		if i == n-1:
+			v_r = -v_c.x
+		if j == 0:
+			v_d = -v_c.y
+		if j == n-1:
+			v_u = -v_c.y
+
+		divergences[i, j] = (v_r - v_l + v_u - v_d) / (2*dx)
+
+
+@ti.kernel
+def pressure_jacobi(pressures:ti.template(), new_pressures:ti.template()):
+
+	for i, j in velocities:
+		c = ti.Vector([i + stagger.x, j + stagger.y]) * dx
+		l = c - ti.Vector([1, 0]) * dx
+		r = c + ti.Vector([1, 0]) * dx
+		d = c - ti.Vector([0, 1]) * dx
+		u = c + ti.Vector([0, 1]) * dx
+
 		p_l = sample_bilinear(pressures, l)
 		p_r = sample_bilinear(pressures, r)
 		p_d = sample_bilinear(pressures, d)
 		p_u = sample_bilinear(pressures, u)
 
-		
-		div_v = (v_r - v_l + v_u - v_d) / dx
+		new_pressures[i, j] = ( p_l + p_r + p_d + p_u - divergences[i, j] * rho / dt * (dx*dx) ) * 0.25
 
-		k = 4
-		if i == 0: 
-			v_l = -v_c.x
-			# div_v = (v_r - v_c.x) / (dx/2) + (v_u - v_d) / dx
-			# p_l = 0.0
-			# k -= 1
-		if i == n-1:
-			v_r = -v_c.x
-			# div_v = (v_c.x - v_l) / (dx/2) + (v_u - v_d) / dx
-			# p_r = 0.0
-			# k -= 1
-		if j == 0:
-			v_d = -v_c.y
-			# div_v = (v_r - v_l) / dx + (v_u - v_c.y) / (dx/2)
-			# p_d = 0.0
-			# k -= 1
-		if j == n-1:
-			v_u = -v_c.y
-			# div_v = (v_r - v_l) / dx + (v_c.y - v_d) / (dx/2)
-			# p_u = 0.0
-			# k -= 1
-
-		div_v = (v_r - v_l + v_u - v_d) / dx
-
-
-
-		new_pressures[i, j] = ( p_l + p_r + p_d + p_u - div_v * rho / dt * (dx*dx /4) ) / k
-
-	for i in ti.grouped(pressures):
-		pressures[i] = new_pressures[i]
-
-
-def solve_pressure(num_iter):
-	for i in range(num_iter):
-		jacobi()
 
 
 @ti.kernel
 def projection():
 	for i, j in velocities:
 		c = ti.Vector([i + stagger.x, j + stagger.y]) * dx
-		l = c - ti.Vector([stagger.x, 0]) * dx
-		r = c + ti.Vector([stagger.x, 0]) * dx
-		d = c - ti.Vector([0, stagger.y]) * dx
-		u = c + ti.Vector([0, stagger.y]) * dx
+		l = c - ti.Vector([1, 0]) * dx
+		r = c + ti.Vector([1, 0]) * dx
+		d = c - ti.Vector([0, 1]) * dx
+		u = c + ti.Vector([0, 1]) * dx
 		p_c = sample_bilinear(pressures, c)
 		p_l = sample_bilinear(pressures, l)
 		p_r = sample_bilinear(pressures, r)
 		p_d = sample_bilinear(pressures, d)
 		p_u = sample_bilinear(pressures, u)
 
-		grad_p = ti.Vector([p_r - p_l, p_u - p_d]) / dx
+		grad_p = ti.Vector([p_r - p_l, p_u - p_d]) / (2*dx)
 
 		# if i == 0:
 		# 	grad_p.x = (p_r - p_c) / (dx/2)
@@ -283,9 +249,8 @@ def projection():
 		# if j == n-1:
 		# 	grad_p.y = (p_c - p_d) / (dx/2)
 
-		velocities[i, j] -=  grad_p  / rho * dt
+		velocities[i, j] = velocities[i, j] - grad_p / rho * dt
 
-	# print(velocities[0, 0], colors[0, 0])
 
 @ti.kernel
 def apply_force(pre_mouse_pos:ti.ext_arr(), cur_mouse_pos:ti.ext_arr()):
@@ -296,20 +261,26 @@ def apply_force(pre_mouse_pos:ti.ext_arr(), cur_mouse_pos:ti.ext_arr()):
 	dp = p - pre_p
 	dp = dp / max(1e-5, dp.norm())
 
+	color = (ti.Vector([ti.random(), ti.random(), ti.random()]) * 0.7 + ti.Vector([0.1, 0.1, 0.1]) * 0.3)
+
 	for i, j in velocities:
 
 
 		d2 = (ti.Vector([(i+stagger.x)*dx, (j+stagger.y)*dx]) - p).norm_sqr()
 
-		# if mdir.norm() > 0.5:
-		colors[i, j] += ti.exp(-d2 * (4 / (1 / 15)**2)) * ti.Vector([0.1, 0.1, 0.1])
 
-		# colors[i, j] += ti.Vector([0.1, 0.1, 0.1]) * ti.exp(-d2/0.01) 
-		colors[i, j] *= 0.99
-		velocities[i, j] += dp * dt * ti.exp(-d2/0.01) * 10 
+		velocities[i, j] = velocities[i, j] + dp * dt * ti.exp(-d2/0.001) * 2
 
 
+		if dp.norm() > 0.5:
+			colors[i, j] = colors[i, j] + ti.exp(-d2 * (4 / (1 / 15)**2)) * color
 
+
+
+@ti.kernel
+def decay_color():
+	for i in ti.grouped(colors):
+		colors[i] = colors[i] * 0.99
 
 
 init_color_field()
@@ -326,12 +297,12 @@ pre_mouse_pos = None
 cur_mouse_pos = None
 
 
-for frame in range(30000):
-# while gui.running:	
-	advect(velocities, new_velocities, new_new_velocities, dt)
+for frame in range(450000):
 
+	advect(velocities, new_velocities, new_new_velocities, dt)
 	advect(colors, new_colors, new_new_colors, dt)
-	# # time.sleep(1)
+	velocities, new_velocities = new_velocities, velocities
+	colors, new_colors = new_colors, colors
 
 
 	gui.get_event(ti.GUI.PRESS)
@@ -345,23 +316,27 @@ for frame in range(30000):
 	else:
 		pre_mouse_pos = cur_mouse_pos = None
 
-	if pre_mouse_pos is not None: 
-		# print(pre_mouse_pos, cur_mouse_pos)
-		apply_force(pre_mouse_pos, cur_mouse_pos)
+
+	decay_color()
+
+	solve_divergence()
 
 
-
-	solve_pressure(250)
+	for i in range(jacobi_iters):
+		pressure_jacobi(pressures, new_pressures)
+		pressures, new_pressures = new_pressures, pressures
 
 	projection()
 
+
+
 	gui.set_image(colors)
 
-	gui.text(content=f'RK {RK}', pos=(0, 0.98), color=0x0)
+	gui.text(content=f'RK {RK}', pos=(0, 0.98), color=0xFFFFFF)
 	if enable_BFECC: 
-		gui.text(content=f'BFECC', pos=(0, 0.94), color=0x0)
+		gui.text(content=f'BFECC', pos=(0, 0.94), color=0xFFFFFF)
 		if enable_clipping: 
-			gui.text(content=f'Clipped', pos=(0, 0.90), color=0x0)
+			gui.text(content=f'Clipped', pos=(0, 0.90), color=0xFFFFFF)
 	
 	gui.show()
 
